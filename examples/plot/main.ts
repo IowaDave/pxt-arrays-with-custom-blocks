6e6942239bfb54191fe04d035d33565f9b49d001
Iowa Dave <35478224+IowaDave@users.noreply.github.com>
2019-10-06 09:54:33 -0500

    Update main.ts

--- examples/plot/main.ts
+++ examples/plot/main.ts
@@ -1,9 +1,8 @@
-// place this code in the main.ts file
-// of your project
 let dot1: number[] = []
 dot1 = [2, 2]
 basic.forever(function () {
     custom.turnLED(dot1, OnOff.ON)
-    basic.pause(1000)
+    basic.pause(500)
     custom.turnLED(dot1, OnOff.OFF)
+    basic.pause(500)
 })
