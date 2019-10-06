d7ac7d200f03e46b9a2c8d3431fb1712cf7f8a88
Iowa Dave <35478224+IowaDave@users.noreply.github.com>
2019-10-06 10:23:37 -0500

    Update custom.ts

--- examples/plot/custom.ts
+++ examples/plot/custom.ts
@@ -1,19 +1,47 @@
+/*
+ * Custom block code
+ */
+// Declare this enum outside the custom namespace.
+// Doing so makes it available to regular blocks.
+// It will be used by the custom block inside the namespace.
 enum OnOff {
     ON,
     OFF
 }
 
+// The following code establishes the custom namespace.
+// The custom block is defined inside the namespace.
 /**
  * Custom blocks
  */
 //% weight=100 color=#0fbc11 icon=""
 namespace custom {
+    /*
+     * Function turnLED
+     * Parameters:
+     *    array of 2 numbers,
+     *    selection from an enumerated list
+     * Asserts:
+     *    array has exactly 2 elements of type number
+     * Returns: nothing
+     * Side effects: turns a pixel on or off
+     */ 
+    // The following comment encodes the help-text for the block
     /**
      * Plot or unplot an led on the display
      */
     //% block
     export function turnLED(theDot: number[],
         onOrOff: OnOff): void {
+        // assert that theDot is a two-number array
+        if (
+            theDot.length !== 2
+            || typeof theDot[0] !== "number"   
+        ) {
+            // indicate error and exit
+            basic.showString("?");
+            return;
+        }
         switch (onOrOff) {
             case OnOff.OFF:
                 led.unplot(theDot[0], theDot[1]);
